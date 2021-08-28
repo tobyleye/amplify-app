36caa5363adde58bcb7502d8a2a5cee89f96e3dc
tobi <krisella74@gmail.com>
2021-08-28 13:48:09 +0100

    rolling in the deep lyrics

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { lyrics } from "./lyrics"
 
 function App() {
+  const lines = lyrics.split('\n')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="header">
+      <h3>Rolling in the deep lyrics</h3>
+      <div className="lyrics-source">
+      <span>Source: <a target="_blank" rel="noreferrer" href="http://musixmatch.com">Musixmatch</a></span>
+
+      </div>
+    </header>
+   
+        <div>{
+          lines.map(line => {
+            return (
+              <p>{line}</p>
+            )
+          })
+        }</div>
+    </div>  
   );
 }
 
